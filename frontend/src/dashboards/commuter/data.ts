942b71a7952d3a949d00e03340b21c5285c82cc3
junkerino1 <ooijunkang2005@gmail.com>
2026-07-29 16:59:31 +0800

    removed frontend folder

--- frontend/src/dashboards/commuter/data.ts
+++
@@ -1,7 +0,0 @@
-import { ParkingSpot } from './types';
-
-// TODO: Fetch from backend API
-export const stationsList: string[] = [];
-
-// TODO: Fetch from GET /api/parkingspots/nearby
-export const simulatedSpots: ParkingSpot[] = [];
